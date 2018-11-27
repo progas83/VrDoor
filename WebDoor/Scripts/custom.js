4d5a8aa64eebf1ef66721f0144aac341152c3c34
Stas Barannik <stasbarannik@gmail.com>
2018-11-28 01:45:49 +0200

    desktop design

---
+++ WebDoor/Scripts/custom.js
@@ -0,0 +1,16 @@
+﻿jQuery(document).ready(function ($) {
+var myFullpage = new fullpage('#fullpage', {
+        verticalCentered: false,
+        css3:false
+    });
+
+  $('#games-slider').slick({
+      infinite: true,
+      slidesToShow: 2,
+      autoplay: true,
+      autoplaySpeed: 1400,
+      slidesToScroll: 1,
+      accessibility: true,
+      arrows: false
+    });
+});
